79287049078c5f6c60ac47775b634f79b8f1af7d
as-navaneeth <navaneethsajeev7s@gmail.com>
2025-10-07 15:44:38 +0530

    jsx starting

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 //const heading=React.createElement("h1",{},"hello world form react");
-      //  const root= ReactDOM.createRoot(document.getElementById("root"));
-      //  root.render(heading)
+//  const root= ReactDOM.createRoot(document.getElementById("root"));
+//  root.render(heading)
 
+//React.creatElement=> Object => HTMLElement(render)
+// const heading= React.createElement("h1",{id:"heading"},"React");
 
+const jsxHeading = (
+  <h1 className="head" tabIndex="1">
+    Namaste React using jsx
+  </h1>
+);
 
-const parent=React.createElement("div",{id:"parent"},
-    React.createElement("div",{id:"child"},
-        [React.createElement("h1",{},"Hi I'am h1 tag"),
-            React.createElement('h2',{},"hi I'am h2 tag")
-        ]
-    )
-)
+console.log(jsxHeading);
 
-console.log(parent);
-
-const root=ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(jsxHeading);
